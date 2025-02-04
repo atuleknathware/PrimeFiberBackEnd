e2755855771cbd275b3f633234756ce50e06b192
atul <atulware.22@gmail.com>
2025-02-04 22:36:10 +0530

    Adding Landing page

--- server.js
+++ server.js
@@ -10,7 +10,28 @@ const PORT=process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 // "http://localhost:5173/"
-app.use('/api',AuthRouter)
+app.use('/api',AuthRouter);
+
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Node.js Server</title>
+</head>
+<body>
+    <h1>Welcome to My Simple Node.js Server</h1>
+    <p>This is a basic HTML page served with Express.</p>
+</body>
+</html>
+`;
+
+// Define the route for the landing page
+app.get('/', (req, res) => {
+    res.send(htmlContent);
+});
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
