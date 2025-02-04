d1e79d9d65b0dcef2b72a04e3d7d3064d14b1ebf
Santosh Narawade <santosh.narawade1@gmail.com>
2025-02-04 22:01:38 +0530

    structure changes and added get api for users

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express=require("express")
-const app=express()
-const cors=require("cors")
-const AuthRouter=require('./Routes/AuthRouter')
+import express from "express";
+import cors from "cors";
+import AuthRouter from './Routes/AuthRouter.js';
 
-require("dotenv").config()
-require("./Models/db")
-const PORT=process.env.PORT || 8080;
+const app = express()
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoConnection from "./Models/db.js"
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
 // "http://localhost:5173/"
-app.use('/api',AuthRouter)
+app.use('/api', AuthRouter)
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
